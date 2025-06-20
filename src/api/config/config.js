5ddae9b5c4bbe92c60700631f888a5744bf15106
AndresDev20 <andresmarroquin887@gmail.com>
2025-06-20 18:27:43 -0500

    First commit

---
+++ src/api/config/config.js
@@ -0,0 +1,28 @@
+require('dotenv').config();
+
+module.exports = {
+  development: {
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD || null,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT || 3306,
+    dialect: process.env.DB_DIALECT || 'mysql'
+  },
+  test: {
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD || null,
+    database: process.env.DB_NAME_TEST || 'database_test',
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT || 3306,
+    dialect: process.env.DB_DIALECT || 'mysql'
+  },
+  production: {
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD || null,
+    database: process.env.DB_NAME_PROD || 'database_production',
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT || 3306,
+    dialect: process.env.DB_DIALECT || 'mysql'
+  }
+}
---
+++ src/routers/index.js
@@ -0,0 +1,7 @@
+const express = require('express')
+const router = express.Router()
+
+// Aca colocare mis rutas del proyecto
+
+
+module.exports = router;

977303114005194d22d10b19526539b003dc239e
AndresDev20 <andresmarroquin887@gmail.com>
2025-06-23 19:11:14 -0500

    First commit

--- src/controllers/role.controllers.js
+++ src/controllers/role.controllers.js
@@ -3,14 +3,25 @@ const { Role } = require("../api/models");
 const getAllRoles = async (req, res) => {
   try {
     const roles = await Role.findAll()
-    res.json(roles)
+    return res.json(roles)
   } catch (error) {
     console.error("Error fetching roles", error);
     res.status(500).json({ error: "Internal server error" });
   }
 };
 
+const create = async (req, res) => {
+  const data = req.body;
+  try {
+    const newRole = await Role.create(data)
+    return res.json(newRole)
+  } catch (error) {
+    res.status(500).json({ error: "Internal server error" });
+  }
+}
+
 
 module.exports = {
-    getAllRoles
+    getAllRoles,
+    create
 }

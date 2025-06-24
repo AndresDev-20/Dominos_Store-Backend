675a835b428f958a82c48f9db47ceffd0015a66b
AndresDev20 <andresmarroquin887@gmail.com>
2025-06-24 18:28:37 -0500

    First commit

--- src/controllers/role.controllers.js
+++ src/controllers/role.controllers.js
@@ -2,21 +2,40 @@ const { Role } = require("../api/models");
 const catchError = require("../utils/catchError");
 
 
-
+// Ver todos los roles
 const getAllRoles = catchError(async(req, res) => {
   const roles = await Role.findAll();
   return res.json(roles)
 })
 
-
+// Crear un rol
 const create = catchError(async(req, res) => {
   let newRole = req.body;
   const addRole = await Role.create(newRole)
   return res.json({message: "El rol se creo exitosamente", addRole})
 })
 
+//Eliminar un rol por id
+const remove = catchError(async(req, res) => {
+  const { id } = req.params;
+  const role = await Role.destroy({where: {id}})
+  if(role !== 1) return res.status(404).json({error:"Rol no encontrado"})
+  return res.status(204).send()
+})
+
+//Actualizar por id
+const update = catchError(async(req, res) => {
+  const { id } = req.params;
+  const data = req.body;
+  const roleUpdate = await Role.update(data, {where: {id}})
+  if(roleUpdate[0] === 0) return res.status(404).json({error: "El rol no se actualizo porque no existe en la base de datos"})
+  return res.json({menssage: "El rol se actualizo exitosamente"});
+})
+
 
 module.exports = {
     getAllRoles,
-    create
+    create,
+    remove,
+    update
 }
--- src/routers/role.router.js
+++ src/routers/role.router.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { getAllRoles, create } = require('../controllers/role.controllers');
+const { getAllRoles, create, remove, update } = require('../controllers/role.controllers');
 
 
 const roleRouter = express.Router();
@@ -7,6 +7,10 @@ const roleRouter = express.Router();
 roleRouter.route('/')
           .get(getAllRoles)
           .post(create)
+
+roleRouter.route('/:id')
+          .delete(remove)
+          .put(update)
 
 
 

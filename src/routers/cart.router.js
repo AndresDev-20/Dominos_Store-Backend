b44aad4babda7b176fe6f9774620a102cba11bc9
AndresDev20 <andresmarroquin887@gmail.com>
2025-07-23 16:32:06 -0500

    Firts commit

--- src/routers/cart.router.js
+++ src/routers/cart.router.js
@@ -1,11 +1,14 @@
 const express = require('express');
-const { getAllCarts } = require('../controllers/car.controllers');
+const { getAllCarts, createCart, updateCart, deleteCart } = require('../controllers/car.controllers');
 
 
 const cartRouter = express.Router();
 
 cartRouter.route('/')
                 .get(getAllCarts)
+                .post(createCart)
+                .put(updateCart)
+                .delete(deleteCart);
 
 
 module.exports = cartRouter;

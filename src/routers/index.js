7f6e9397bbfbc0fe52d9166cb94f14f0adb52d84
AndresDev20 <andresmarroquin887@gmail.com>
2025-11-28 15:42:50 -0500

    Dominos

--- src/routers/index.js
+++ src/routers/index.js
@@ -5,11 +5,13 @@ const productRouter = require('./product.router');
 const cartRouter = require('./cart.router');
 const detailCartRouter = require('./detailcart.router');
 const imageRouter = require('./image.router');
+const categoryRouter = require('./category.router');
 const router = express.Router()
 
 // Aca colocare mis rutas del proyecto
 router.use("/roles", roleRouter)
 router.use('/users', userRouter)
+router.use('/categories', categoryRouter)
 router.use('/products', productRouter)
 router.use('/images', imageRouter)
 router.use('/carts', cartRouter)

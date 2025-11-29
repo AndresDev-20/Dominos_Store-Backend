d2bd580e390e05508dc62f26c417f27756753a13
AndresDev20 <andresmarroquin887@gmail.com>
2025-11-29 15:54:31 -0500

    Dominos

--- src/routers/category.router.js
+++ src/routers/category.router.js
@@ -1,10 +1,11 @@
 const express = require('express')
-const { getAllCategories } = require("../controllers/category.controllers")
+const { getAllCategories, createCategory } = require("../controllers/category.controllers")
 
 const categoryRouter = express.Router();
 
 categoryRouter.route("/")
               .get(getAllCategories)
+              .post(createCategory)
 
 
 module.exports = categoryRouter

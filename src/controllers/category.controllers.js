9d2a9d348cebc0b85b6fa98d08465f5bdd34daba
AndresDev20 <andresmarroquin887@gmail.com>
2025-12-28 15:33:40 -0500

    Logic

--- src/controllers/category.controllers.js
+++ src/controllers/category.controllers.js
@@ -1,6 +1,13 @@
 
+const { Category } = require('../api/models')
+const catchError = require('../utils/catchError')
 
+// Obtener todos las categorias
+const getAllCategories = catchError(async (req, res) => {
+    const categories = await Category.findAll();
+    return res.status(200).json(categories)
+})
 
 module.exports = {
-
+    getAllCategories,
 }
